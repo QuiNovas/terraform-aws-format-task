ca42cbda061416214c782650e0073c5eea73a5e7
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2019-04-15 16:34:41 +0530

    no policies needed

--- main.tf
+++ main.tf
@@ -5,12 +5,6 @@ module "format_task" {
   kms_key_arn     = "${var.kms_key_arn}"
   l3_object_key   = "quinovas/format-task/format-task-0.0.1.zip"
   name            = "${var.name_prefix}format-task"
-
-  policy_arns = [
-    "${aws_iam_policy.format_task.arn}",
-  ]
-
-  policy_arns_count = 1
   runtime           = "python3.7"
   source            = "QuiNovas/lambdalambdalambda/aws"
   timeout           = 5
--- outputs.tf
+++ outputs.tf
@@ -1,30 +1,30 @@
 output "arn" {
-  description = "The arn of parameter store task Lambda function"
-  value       = "${module.parameter_store_task.arn}"
+  description = "The arn of format task Lambda function"
+  value       = "${module.format_task.arn}"
 }
 
 output "invoke_arn" {
   description = "The invocation arn of this task Lambda function"
-  value       = "${module.parameter_store_task.invoke_arn}"
+  value       = "${module.format_task.invoke_arn}"
 }
 
 output "invoke_policy_arn" {
-  description = "The arn of the invocation policy parameter store task Lambda function"
-  value       = "${module.parameter_store_task.invoke_policy_arn}"
+  description = "The arn of the invocation policy format task Lambda function"
+  value       = "${module.format_task.invoke_policy_arn}"
 }
 
 output "name" {
   description = "The name of the this Lambda function"
-  value       = "${module.parameter_store_task.name}"
+  value       = "${module.format_task.name}"
 }
 
 output "qualified_arn" {
   description = "The qualified arn of this Lambda function"
-  value       = "${module.parameter_store_task.qualified_arn}"
+  value       = "${module.format_task.qualified_arn}"
 }
 
 output "qualified_invoke_arn" {
   description = "The qualified invocation arn of the Lambda function"
-  value       = "${module.parameter_store_task.qualified_invoke_arn}"
+  value       = "${module.format_task.qualified_invoke_arn}"
 }
 
