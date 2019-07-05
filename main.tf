3b97f265be1d86bf2976a900baea73f4f746ab80
rajhansh-seyfert <kumarrajhanshgit@gmail.com>
2019-07-05 15:01:31 +0530

    updated to terraform 12

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "format_task" {
-
-  dead_letter_arn = "${var.dead_letter_arn}"
+  dead_letter_arn = var.dead_letter_arn
   handler         = "function.handler"
-  kms_key_arn     = "${var.kms_key_arn}"
+  kms_key_arn     = var.kms_key_arn
   l3_object_key   = "quinovas/format-task/format-task-0.0.1.zip"
   name            = "${var.name_prefix}format-task"
-  runtime           = "python3.7"
-  source            = "QuiNovas/lambdalambdalambda/aws"
-  timeout           = 5
-  version           = "0.2.0"
+  runtime         = "python3.7"
+  source          = "QuiNovas/lambdalambdalambda/aws"
+  timeout         = 5
+  version         = "3.0.0"
 }
+
